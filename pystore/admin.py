f16711f1cc945e596e1a26a54145475d9d193b3e
bonelli95 <rodrigobonelli95@gmail.com>
2024-07-02 09:56:20 +0100

    e-commerce

---
+++ pystore/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from pystore.models import Category, Brand, Size, Color, Product
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Size)
+admin.site.register(Color)
+admin.site.register(Product)
+
---
+++ pystore/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, null=False, blank=True, default="")
+    image = models.ImageField(upload_to='cat_photos/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Brand(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=True, default="")
+    image = models.ImageField(upload_to='brand_photos/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Color(models.Model):
+    title = models.CharField(max_length=50)
+    color_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Size(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='product_photos/', blank=True)
+    slug = models.CharField(max_length=250)
+    detail = models.TextField()
+    specification = models.TextField()
+    price = models.PositiveIntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
