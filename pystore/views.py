dccf841b65f44a4b9a1f923955c877f387ccfa88
bonelli95 <rodrigobonelli95@gmail.com>
2024-07-03 09:19:11 +0100

    created front-end

--- pystore/views.py
+++ pystore/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from pystore.models import Banner, Category, Brand, Color, Size, Product, ProductAttribute
 
 def index(request):
-    return render(request, 'pystore/index.html')
+    return render(request, 'index.html')
