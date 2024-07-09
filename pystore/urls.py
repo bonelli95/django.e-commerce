560720bd964d0967daad2e2f6f2ba9e42dda6584
bonelli95 <rodrigobonelli95@gmail.com>
2024-07-09 16:54:52 +0100

    update

--- pystore/urls.py
+++ pystore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pystore.views import index, categories, brand, product
+from pystore.views import index, categories, brand, product, category_product_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
     path('brand/', brand, name='brand'),
     path('product/', product, name='product'),
+    path('category-product-list/<int:id>', category_product_list, name='category_product_list'),
 ]
--- pystore/views.py
+++ pystore/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from pystore.models import Banner, Category, Brand, Color, Size, Product, ProductAttribute
 
 def index(request):
-    return render(request, 'index.html')
+    data = Product.objects.filter(is_featured_product = True).order_by('-id')
+    return render(request, 'index.html', {'data': data})
 
 def categories(request):
     data = Category.objects.all().order_by('-id')
@@ -14,6 +15,7 @@ def brand(request):
 
 def product(request):
     data = Product.objects.all().order_by('-id')
+    dataAtt = data.prefetch_related('productattribute_set')
 
     category = Product.objects.distinct().values('category__title', 'category__id')
     brands = Product.objects.distinct().values('brand__title', 'brand__id')
@@ -26,5 +28,11 @@ def product(request):
         'category': category,
         'brands': brands,
         'color': color,
-        'sizes': sizes
+        'sizes': sizes,
+        'dataAtt':dataAtt,
     })
+
+def category_product_list(request, cat_id):
+  category = Category.objects.get(id=cat_id)
+  data = Product.objects.filter(category=category).order_by('-id')
+  return render(request, 'category_product_list.html', {'data': data})
