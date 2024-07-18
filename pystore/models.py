384c053788101ffc4905095407d1987efdfd7bb6
Rodrigo <rodrigobonelli95@gmail.com>
2024-07-18 21:45:28 +0100

    changes

--- pystore/models.py
+++ pystore/models.py
@@ -58,7 +58,7 @@ class ProductAttribute(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
-    price = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.product.title
