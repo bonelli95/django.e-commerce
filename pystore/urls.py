eea207ebc9fc5f848347a451111197eab63f9ed6
Rodrigo <rodrigobonelli95@gmail.com>
2024-08-07 14:00:34 +0100

    creating authentication pages and forms

--- pystore/urls.py
+++ pystore/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from pystore.views import index, categories, brand, product, category_product_list
+from pystore.views import index, categories, product, category_product_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
-    path('brand/', brand, name='brand'),
     path('product/', product, name='product'),
     path('category_product_list/<int:cat_id>', category_product_list, name='category_product_list'),
 
